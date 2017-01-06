970718d4bf8bbad87e313191d4d8577330a08c57
Bruno Couto <bruno.couto@bluewin.ch>
2017-01-06 12:13:08 +0100

    Some tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {initializeApp, database} from 'firebase';
 import {firebaseConfig} from '../environments/firebase.config';
-import {AngularFire} from "angularfire2";
+import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from "angularfire2";
 
 @Component({
   selector: 'app-root',
@@ -11,8 +11,23 @@ import {AngularFire} from "angularfire2";
 export class AppComponent {
   title = 'app works fine!';
 
+  publishers$:FirebaseListObservable<any>;
+  reports$:FirebaseObjectObservable<any>;
+
   constructor(private af: AngularFire) {
 
-    
+    this.publishers$ = af.database.list('publishers');
+
+    this.publishers$.subscribe(console.log);
+
+    this.reports$ = af.database.object('reports/-K_nBy_7UpL11GtYmzxt');
+
+    this.reports$.subscribe(console.log);
+
+  }
+
+  listPush() {
+
+    this.publishers$.push({firstName: 'TEST'});
   }
 }
